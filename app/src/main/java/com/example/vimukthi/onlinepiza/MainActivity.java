eeea3035af28a993472a37e2006aa35850dd72ba
vimukthi96 <vimukthi072@gmail.com>
2018-10-11 21:43:45 +0530

    Second Commit

--- app/src/main/java/com/example/vimukthi/onlinepiza/MainActivity.java
+++ app/src/main/java/com/example/vimukthi/onlinepiza/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.vimukthi.onlinepiza;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnSignIn,btnSignUp;
+    private TextView txtView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn=(Button)findViewById(R.id.btnSignIn);
+        btnSignUp=(Button)findViewById(R.id.btnSignUp);
+        txtView =(TextView)findViewById(R.id.txtLogin);
+
+        Typeface typeface=Typeface.createFromAsset(getAssets(),"fonts/NABILA.TTF");
+        txtView.setTypeface(typeface);
+       btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent =new Intent(MainActivity.this,Sign_in.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
