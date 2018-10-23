04ab3c7fcb5c678a66fbc5facdefebfa6328d9a5
vimukthi96 <vimukthi072@gmail.com>
2018-10-23 11:26:10 +0530

    Change something in issues

--- app/src/main/java/com/example/vimukthi/onlinepiza/HomeActivity.java
+++ app/src/main/java/com/example/vimukthi/onlinepiza/HomeActivity.java
@@ -1,172 +1,66 @@
 package com.example.vimukthi.onlinepiza;
 
-import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.vimukthi.onlinepiza.Common.Common;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.squareup.picasso.Picasso;
-import static com.example.vimukthi.onlinepiza.R.layout.menu_item;
-import static java.lang.System.load;
 
-public class HomeActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-
+public class HomeActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
     FirebaseDatabase firebaseDatabase;
-    DatabaseReference category;
-    TextView txtFullName;
-    RecyclerView recyclerViewMenu;
-    RecyclerView.LayoutManager layoutManager;
-
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("Menu");
-        setSupportActionBar(toolbar);
+        recyclerView =findViewById(R.id.recycleView);
+        recyclerView.setHasFixedSize(true);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         firebaseDatabase =FirebaseDatabase.getInstance();
-        category =firebaseDatabase.getReference("Category");
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        View headerView =navigationView.getHeaderView(0);
-        txtFullName=(TextView)findViewById(R.id.txtFullName);
-        txtFullName.setText(Common.currentUser.getName());
-
-        recyclerViewMenu=(RecyclerView)findViewById(R.id.recyclerViewContent);
-        recyclerViewMenu.setHasFixedSize(true);
-        layoutManager =new LinearLayoutManager(this);
-        recyclerViewMenu.setLayoutManager(layoutManager);
-
-        loadMenu();
-
+        databaseReference =firebaseDatabase.getReference("Category");
 
     }
 
-    private void loadMenu() {
-        FirebaseRecyclerOptions<Category> options =
-                new FirebaseRecyclerOptions.Builder<Category>()
-                        .setQuery(category, Category.class)
-                        .build();
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseRecyclerAdapter<MenuItems,MenuViewHolder> firebaseRecyclerAdapter =new FirebaseRecyclerAdapter<MenuItems,MenuViewHolder>(
+                MenuItems.class,R.layout.meu_item,MenuViewHolder.class,databaseReference) {
 
-        FirebaseRecyclerAdapter<Category,MenuViewHolder> adapter=new FirebaseRecyclerAdapter<Category,MenuViewHolder>(Category.class,R.layout.menu_item,MenuViewHolder.class,category){
-
-            @NonNull
             @Override
-            public MenuViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                return null;
+            protected void populateViewHolder(MenuViewHolder menuViewHolder, MenuItems menuItems, int position) {
+                menuViewHolder.setDetails(getApplicationContext(),menuItems.getName(),menuItems.getImage());
             }
 
             @Override
-            protected void onBindViewHolder(@NonNull MenuViewHolder holder, int position, @NonNull Category model) {
-                holder.txtMenuName.setText(model.getName());
-                Picasso.with(getBaseContext()).load(model.getImage()).into(holder.menuImage);
-                final Category clickItem = model;
+            public MenuViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                MenuViewHolder menuViewHolder = super.onCreateViewHolder(parent,viewType);
+                menuViewHolder.setClickListner(new MenuViewHolder.ItemClickListner() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+
+                    }
+
+                    @Override
+                    public void onItemLongClick(View view, int position) {
+
+                    }
+                });
+
+                return super.onCreateViewHolder(parent, viewType);
             }
-
-//            @Override
-//            protected void populateViewHolder(MenuViewHolder viewHolder, Category model, int position) {
-
-//                viewHolder.setItemClickListener(new ItemClickListner() {
-//                    @Override
-//                    public void onClick(View view, int position, boolean isLongClick) {
-//                        Toast.makeText(HomeActivity.this,""+clickItem.getName(),Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-  //          }
         };
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_cart) {
-            // Handle the camera action
-        } else if (id == R.id.nav_log_out) {
-
-        } else if (id == R.id.nav_menu) {
-
-        } else if (id == R.id.nav_order) {
-
-        } else if (id == R.id.nav_view) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+        recyclerView.setAdapter(firebaseRecyclerAdapter);
     }
 }
