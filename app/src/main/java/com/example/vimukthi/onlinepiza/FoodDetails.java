223d224a77e3b3aa52c40eef7c02b15870e28b75
vimukthi96 <vimukthi072@gmail.com>
2018-11-02 21:47:46 +0530

    small changes

---
+++ app/src/main/java/com/example/vimukthi/onlinepiza/FoodDetails.java
@@ -0,0 +1,13 @@
+package com.example.vimukthi.onlinepiza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoodDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_details);
+    }
+}
